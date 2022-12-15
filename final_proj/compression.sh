c41dd6b32ae3b457a11b83f15a34b42b3f622ff5
pranayvarshney <pranay.varshney9@gmail.com>
2022-12-15 19:51:44 +0530

    fixed script a bit

--- final_proj/compression.sh
+++ final_proj/compression.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Set the directory where the files to be compressed are located
-source_dir="/path/to/files/to/compress"
+source_dir="/home/pranay/Documents/DSTN/assignments/final_proj/dstn-geofen-storage/data"
 
 # Set the directory where the compressed files should be saved
-target_dir="/path/to/compressed/files"
+target_dir="/home/pranay/Documents/DSTN/assignments/final_proj/dstn-geofen-storage/compressed"
 
 # Set the filename for the compressed file
 today=$(date +%Y-%m-%d)
-filename="$today-backup.bz2"
+filename="$today.bz2"
 
 # Compress the files in the source directory and save the compressed file in the target directory
-pbzip2 -c -z3 "$source_dir" > "$target_dir/$filename"
+pbzip2 -c -z3 "$source_dir/$today" > "$target_dir/$filename"
 
 # Schedule a cron job to run the script at midnight every day
 #(crontab -l 2>/dev/null; echo "0 0 * * * $PWD/$0") | crontab -
+#30 23 * * * /path/compresssion.sh
